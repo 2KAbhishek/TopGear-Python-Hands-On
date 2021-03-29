a09034bb8334aad7c2087ff1eb8030d1d201c8f6
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-03-30 03:00:31 +0530

    Add program4.py

---
+++ Programs/program4.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+a = 7
+for i in range(1, a-1):
+    if a % i == 0:
+        print("The given number", a, "is not prime")
+        break
+    else:
+        print("The given number ", a, "is prime")
+        break
